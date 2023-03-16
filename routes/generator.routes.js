ab79f1e7e3355930a0ace1f07e27006e97f26e6d
André Lisboa <andrelisboapt@hotmail.com>
2023-03-16 23:02:09 +0000

    cloudinary bug

--- routes/generator.routes.js
+++ routes/generator.routes.js
@@ -13,12 +13,12 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-process.env.CLOUDINARY_SECURE = true;
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-  
+  secure: true,
 });
 
 
