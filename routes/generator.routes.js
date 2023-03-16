cbc947b1436805d806b30028536d11dc97b33809
André Lisboa <andrelisboapt@hotmail.com>
2023-03-16 23:39:16 +0000

    bug fixed

--- routes/generator.routes.js
+++ routes/generator.routes.js
@@ -29,7 +29,7 @@ router.post("/generator", isAuthenticated, async (req, res, next) => {
   try {
     const response = await openai.createImage({
       prompt: search,
-      n: 2,
+      n: 4,
       size: "256x256",
     });
 
@@ -37,9 +37,15 @@ router.post("/generator", isAuthenticated, async (req, res, next) => {
     const resultUrl = cloudinary.url(result.public_id, {secure: true});
     const result1 = await cloudinary.uploader.upload(response.data.data[1].url, {secure: true});
     const resultUrl1 = cloudinary.url(result1.public_id, {secure: true});
+    const result2 = await cloudinary.uploader.upload(response.data.data[2].url, {secure: true});
+    const resultUrl2 = cloudinary.url(result2.public_id, {secure: true});
+    const result3 = await cloudinary.uploader.upload(response.data.data[3].url, {secure: true});
+    const resultUrl3 = cloudinary.url(result3.public_id, {secure: true});
 
     const createdImg = await Image.create({ imageURL: resultUrl });
     const createdImg1 = await Image.create({ imageURL: resultUrl1 });
+    const createdImg2 = await Image.create({ imageURL: resultUrl2 });
+    const createdImg3 = await Image.create({ imageURL: resultUrl3 });
    
     /*  const userUpdated = await User.findByIdAndUpdate(_id, {
       $push: { gallery: updatedImg },
@@ -51,7 +57,7 @@ router.post("/generator", isAuthenticated, async (req, res, next) => {
 
     
 
-    res.json([createdImg, createdImg1]);
+    res.json([createdImg, createdImg1, createdImg2, createdImg3]);
   } catch (error) {
     res.json(error);
   }
